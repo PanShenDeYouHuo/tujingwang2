27011c8c37ee99ae27804f9489f1aed2834fdcaf
PanShenDeYouHuo <243080462@qq.com>
2017-10-17 21:37:21 +0800

    wechat3

--- app/routers/login.js
+++ app/routers/login.js
@@ -5,11 +5,10 @@ let routers = router;
 module.exports = (io)=> {
     console.log(io.sockets);
     routers.get('/wechat', async(ctx)=> {
-        io.on("connection",function(socket){
+        // io.on("connection",function(socket){
             console.log(ctx.query.state);
             io.to(ctx.query.state).emit('wechatok','surprise');
-        });
-        ctx.body = 'wechat';
+        // });
     });
 
     return routers;
