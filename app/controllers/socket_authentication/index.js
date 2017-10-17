24ae26aa5d6e99dbdbd12373d3feecc2b38ab9bf
PanShenDeYouHuo <243080462@qq.com>
2017-10-17 15:08:58 +0800

    微信登入0.1

--- app/controllers/socket_authentication/index.js
+++ app/controllers/socket_authentication/index.js
@@ -20,44 +20,43 @@ module.exports = function (secret) {
 		if (query.token) {
 			if (token.jwtAuthentication(query.token, secret)) {
 				let user = token.jwtParse(query.token);
-				console.log(` ${user.payload.name} token验证通过`);
 				socket.name = user.payload.name;
 				socket.token = query.token;
-				return next();
+				console.log(` ${user.payload.name} token验证通过`);
 			}
-			return next(new Error(`登入过期`));
 		}
-		//账号验证
-		else if (query.account && query.password) {
-			console.log(`账号:${query.account},密码:${query.password}`);
+		// //账号验证
+		// else if (query.account && query.password) {
+		// 	console.log(`账号:${query.account},密码:${query.password}`);
 
-			Employee_mongodb.getEmployeeOne({mobilePhoneNumber: query.account})
-				.then((res)=> {
-					return verification(res, query.password);
-				})
-				.then((res)=> {
-					let header = {
-						typ: "JWT",
-						alg: "HS256"
-					};
-					let payload = {
-						iss: "pan.com",
-						exp: "1438956778",
-						name: res.name,
-						accessAuthority: 1
-					};
-					socket.name = res.name;
-					socket.token = token.jwtSignature(JSON.stringify(header), JSON.stringify(payload), secret); //生成token，挂在到socket上
-					return next();
-				})
-				.catch((err)=> {
-					return next(new Error(err));
-				});
-		}
-		//错误的链接
-		else {
-			return next(new Error(`登入错误`));
-		}
+		// 	Employee_mongodb.getEmployeeOne({mobilePhoneNumber: query.account})
+		// 		.then((res)=> {
+		// 			return verification(res, query.password);
+		// 		})
+		// 		.then((res)=> {
+		// 			let header = {
+		// 				typ: "JWT",
+		// 				alg: "HS256"
+		// 			};
+		// 			let payload = {
+		// 				iss: "pan.com",
+		// 				exp: "1438956778",
+		// 				name: res.name,
+		// 				accessAuthority: 1
+		// 			};
+		// 			socket.name = res.name;
+		// 			socket.token = token.jwtSignature(JSON.stringify(header), JSON.stringify(payload), secret); //生成token，挂在到socket上
+		// 			return next();
+		// 		})
+		// 		.catch((err)=> {
+		// 			return next(new Error(err));
+		// 		});
+		// }
+		// //错误的链接
+		// else {
+		// 	return next(new Error(`登入错误`));
+		// }
+		next();
 
 	}
 }
