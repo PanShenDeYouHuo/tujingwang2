6889bf3733cbbab49a90746ab9253525ea273092
PanShenDeYouHuo <243080462@qq.com>
2017-10-20 14:29:36 +0800

    增加request模块

--- app/routers/login.js
+++ app/routers/login.js
@@ -1,10 +1,13 @@
 const router = require('koa-router')();
+const request = Promise.promisify(require('request'));
 
 let routers = router;
 
 module.exports = (io)=> {
     routers.get('/wechat', async(ctx)=> {
         io.to(ctx.query.state).emit('wechatok','surprise');
+
+        //成功后关闭
         let html = `
             <script type="text/javascript">
                 self.close()
