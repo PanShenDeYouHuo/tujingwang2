61e382ebeb4956a6bb4b5613029061adde13b128
PanShenDeYouHuo <243080462@qq.com>
2017-10-20 11:44:48 +0800

    wechat3.1

--- app/routers/login.js
+++ app/routers/login.js
@@ -5,7 +5,7 @@ let routers = router;
 module.exports = (io)=> {
     routers.get('/wechat', async(ctx)=> {
         io.to(ctx.query.state).emit('wechatok','surprise');
-        let html = `<script language="java script"> 
+        let html = `<script language="text/javascript"> 
                             setTimeout("self.close()",1000)
                     </script> `
         ctx.body = html;
