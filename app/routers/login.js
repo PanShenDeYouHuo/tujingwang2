3bbad014adc326297cef577b80f3695954b8e18e
PanShenDeYouHuo <243080462@qq.com>
2017-10-20 11:15:01 +0800

    wechat2.8

--- app/routers/login.js
+++ app/routers/login.js
@@ -3,12 +3,9 @@ const router = require('koa-router')();
 let routers = router;
 
 module.exports = (io)=> {
-    console.log(io.sockets);
     routers.get('/wechat', async(ctx)=> {
-        // io.on("connection",function(socket){
-            console.log(ctx.query.state);
-            io.to(ctx.query.state).emit('wechatok','surprise');
-        // });
+        io.to(ctx.query.state).emit('wechatok','surprise');
+        ctx.body = "";
     });
 
     return routers;
