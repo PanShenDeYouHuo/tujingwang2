0b47e17e935ebec4ccf373370c228d7034361b9b
PanShenDeYouHuo <243080462@qq.com>
2017-10-21 19:33:54 +0800

    w l 2.19

--- app/controllers/user/login.js
+++ app/controllers/user/login.js
@@ -13,36 +13,37 @@ function Login() {
  */
 Login.prototype.wechat = ()=> {
     return async(ctx)=> {
-        console.log(`appid: ${wechat.appId}, appSecret: ${wechat.appSecret}`)
-                let body = await wechat.getAccessToken(wechat.appId, wechat.appSecret, ctx.query.code);
-                if(body.errcode) {
-                    console.log(body.errmsg);
-                } else {
-                    console.log(`access_token:${body.access_token}`);        
-                    console.log(`expires_in:${body.expires_in}`);        
-                    console.log(`refresh_token:${body.refresh_token}`);        
-                    console.log(`openid:${body.openid}`);        
-                    console.log(`scope:${body.scope}`);        
-                }
-        
-                let user = await wechat.getUnionId(body.access_token, body.openId);
-                if(body.errcode) {
-                    console.log(body.errmsg)
-                } else {
-                    console.log(user);
-                }
-        
-        
-        
-                sio.to(ctx.query.state).emit('wechatok','surprise');
-        
-                //成功后关闭
-                let html = `
-                    <script type="text/javascript">
-                        self.close()
-                    </script> 
-                `
-                ctx.body = html;
+        // console.log(`appid: ${wechat.appId}, appSecret: ${wechat.appSecret}`)
+        console.log(`code: ${ctx.query.code}`);
+        let body = await wechat.getAccessToken(wechat.appId, wechat.appSecret, ctx.query.code);
+        if(body.errcode) {
+            console.log(body.errmsg);
+        } else {
+            console.log(`access_token:${body.access_token}`);
+            console.log(`expires_in:${body.expires_in}`);
+            console.log(`refresh_token:${body.refresh_token}`);
+            console.log(`openid:${body.openid}`);
+            console.log(`scope:${body.scope}`);
+        }
+
+        let user = await wechat.getUnionId(body.access_token, body.openId);
+        if(body.errcode) {
+            console.log(body.errmsg)
+        } else {
+            console.log(user);
+        }
+
+
+
+        sio.to(ctx.query.state).emit('wechatok','surprise');
+
+        //成功后关闭
+        let html = `
+            <script type="text/javascript">
+                self.close()
+            </script> 
+        `
+        ctx.body = html;
     }
 }
 
