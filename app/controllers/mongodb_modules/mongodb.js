6845f36ee0af76b98af290ebdd2ab8eed2e0b495
PanShenDeYouHuo <243080462@qq.com>
2017-10-21 19:11:56 +0800

    w l 2.19

--- app/controllers/mongodb_modules/mongodb.js
+++ app/controllers/mongodb_modules/mongodb.js
@@ -1,4 +1,5 @@
-var mongoose = require("mongoose");
+const debug = require('debug')('tujing:mongodb');
+const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 //////// 连接 ////////
@@ -7,17 +8,17 @@ mongoose.connect(db_url, {useMongoClient: true});
 
 //////// 连接成功 ////////
 mongoose.connection.on("connected", function(){
-	console.log("mongoose connection open to" + db_url);
+	debug("mongoose connection open to" + db_url);
 });
 
 //////// 连接异常 ////////
 mongoose.connection.on("error", function(err){
-	console.log("mongoose connection error:" + err);
+	debug("mongoose connection error:" + err);
 });
 
 //////// 连接断开 ////////
 mongoose.connection.on("disconnected", function(){
-	console.log("mongoose connection  disconnected");
+	debug("mongoose connection  disconnected");
 });
 
 module.exports = mongoose;
