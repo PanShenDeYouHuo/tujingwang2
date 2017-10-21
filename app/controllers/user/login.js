e8cc7e708489842924a91f9fce66e3cac690801e
PanShenDeYouHuo <243080462@qq.com>
2017-10-21 20:44:18 +0800

    ceshi

--- app/controllers/user/login.js
+++ app/controllers/user/login.js
@@ -17,21 +17,24 @@ Login.prototype.wechat = ()=> {
         let body = await wechat.getAccessToken(wechat.appId, wechat.appSecret, ctx.query.ode);
         body = JSON.parse(body);
         if(body.errcode) {
-            console.log(body.errmsg);
-        } else {
-            console.log(`access_token:${body.access_token}`);
-            console.log(`expires_in:${body.expires_in}`);
-            console.log(`refresh_token:${body.refresh_token}`);
-            console.log(`openid:${body.openid}`);
-            console.log(`scope:${body.scope}`);
+            sio.to(ctx.query.state).emit('error', 'body.errmsg');
+            return console.log(body.errmsg);
         }
+
+        console.log(`access_token:${body.access_token}`);
+        console.log(`expires_in:${body.expires_in}`);
+        console.log(`refresh_token:${body.refresh_token}`);
+        console.log(`openid:${body.openid}`);
+        console.log(`scope:${body.scope}`);
+        
 
         let user = JSON.parse(await wechat.getUnionId(body.access_token, body.openid));
         if(user.errcode) {
             console.log(user.errmsg)
-        } else {
-            console.log(user);
         }
+
+        console.log(user);
+        
 
 
 
