67a586dbdc51a20c3d583ee6c1906a78c763d83d
PanShenDeYouHuo <243080462@qq.com>
2017-10-23 19:49:12 +0800

    ceshi

--- app/socket_routers/index.js
+++ app/socket_routers/index.js
@@ -16,34 +16,34 @@ module.exports = ()=> {
 
 
 		//登入认证接口，根据权限开通socket接口
-		socket.on('login', async (accessToken)=> {
+		socket.on('login', (accessToken)=> {
 			try {
 				console.log(hello); 
-				if(!token.jwtAuthentication(accessToken, 'meihaodeshijie,meihaodeshenghuo')) return;
+				// if(!token.jwtAuthentication(accessToken, 'meihaodeshijie,meihaodeshenghuo')) return;
 
-				let token = token.jwtParse(accessToken);
-				socket.userId = token.payload._id;
-				socket.authority = token.payload.authority;
+				// let token = token.jwtParse(accessToken);
+				// socket.userId = token.payload._id;
+				// socket.authority = token.payload.authority;
 
-				//检查是否过期
-				let isSame = await user_db.findById(token.payload._id);
-				if(isSame != accessToken) return;
+				// //检查是否过期
+				// let isSame = await user_db.findById(token.payload._id);
+				// if(isSame != accessToken) return;
 
-				console.log(isSame != accessToken); 
+				// console.log(isSame != accessToken); 
 				
-				//对接口进行权限设置
-				switch (token.payload.authority) {
-					case 1:
-						user(socket);
-						break;
+				// //对接口进行权限设置
+				// switch (token.payload.authority) {
+				// 	case 1:
+				// 		user(socket);
+				// 		break;
 				
-					default:
-						break;
-				}
+				// 	default:
+				// 		break;
+				// }
 				
-				//登入成功返回最新数据
-				let account = await user_db.findOne({'_id': token.payload._id}, {'authority': 1, 'accessToken': 1, 'nickname': 1, 'sex': 1, 'province': 1, 'city': 1, 'country': 1, 'headimgurl': 1,});
-				socket.emit('loginSuccess', account);
+				// //登入成功返回最新数据
+				// let account = await user_db.findOne({'_id': token.payload._id}, {'authority': 1, 'accessToken': 1, 'nickname': 1, 'sex': 1, 'province': 1, 'city': 1, 'country': 1, 'headimgurl': 1,});
+				// socket.emit('loginSuccess', account);
 			} catch (err) {
 				sio.to(ctx.query.state).emit('error','发生错误');
 				console.log(err);
