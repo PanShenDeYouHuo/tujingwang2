81ce5215c40d537ae6124562775c0557a01c5242
PanShenDeYouHuo <243080462@qq.com>
2017-10-23 20:38:13 +0800

    测试

--- app/socket_routers/index.js
+++ app/socket_routers/index.js
@@ -15,11 +15,11 @@ module.exports = ()=> {
 			console.log('a user disconnected:' + socket.id);
 		});
 
-		socket.emit('error','发生错误');
+		socket.emit('errora','发生错误');
 
 		//登入认证接口，根据权限开通socket接口
 		socket.on('login', (accessToken)=> {
-			socket.emit('error','发生错误');
+			socket.emit('errora','发生错误');
 			// try {
 			// 	// if(!token.jwtAuthentication(accessToken, 'meihaodeshijie,meihaodeshenghuo')) return;
 
