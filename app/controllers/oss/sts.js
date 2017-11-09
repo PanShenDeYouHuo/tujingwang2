11e4ad69d2fe449235c9e437a501a4d366815433
PanShenDeYouHuo <243080462@qq.com>
2017-11-09 18:52:19 +0800

    1

--- app/controllers/oss/sts.js
+++ app/controllers/oss/sts.js
@@ -28,6 +28,8 @@ Sts.prototype.get = async (uid)=> {
 
     let arn = 'acs:ram::1647720766129117:role/tujingcloud-write';
     let sessionName = uid;
+
+    console.log(this.sts);
     
     //获取token
     let token = await this.sts.assumeRole( arn, policy, 1 * 60, sessionName);
