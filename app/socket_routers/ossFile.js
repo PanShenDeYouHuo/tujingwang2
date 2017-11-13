3c579a3245d557be69be7d9d9cd83f11b19aa8ff
PanShenDeYouHuo <243080462@qq.com>
2017-11-13 16:29:31 +0800

    1

--- app/socket_routers/ossFile.js
+++ app/socket_routers/ossFile.js
@@ -5,8 +5,10 @@ module.exports = (socket)=> {
     //获得oss写权限的sts临时token
     socket.on('getReadStsToken', (uid, fu)=>{
         try {
-            console.log(uid);
-            fu(await stsToken.getReadStsToken(uid));
+            console.log(stsToken.getReadStsToken(uid));
+
+            
+            fu(stsToken.getReadStsToken(uid));
         } catch (err) {
             console.log(err);
             socket.volatile.emit('appError','发生错误');
