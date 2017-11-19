8e7e47ff7994ce75678792b9d4a09271a636ad1d
PanShenDeYouHuo <243080462@qq.com>
2017-11-19 11:03:40 +0800

    1

--- app/koa.js
+++ app/koa.js
@@ -5,9 +5,9 @@ const koa = new Koa();
 const routers = require('./routers');										//服务器路由
 
 //初始化静态服务器
-koa.use(require('koa-static')(__dirname + '/public'));                      //静态服务器
+koa.use(require('koa-static')(__dirname + '/public',{ maxage: 3600}));      //静态服务器
 
 // 初始化路由中间件
-koa.use(routers.routes()).use(routers.allowedMethods());
+koa.use(routers.routes()).use(routers.allowedMethods());    
 
 module.exports = koa;
