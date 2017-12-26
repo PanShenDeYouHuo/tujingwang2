1381a6e4c02a8e52e004c0d7f5aebfc6c3f6fa12
panshendeyouhuo <243080462@qq.com>
2017-12-26 15:30:19 +0800

    1

--- app/socket_routers/oss.js
+++ app/socket_routers/oss.js
@@ -1,14 +1,14 @@
 const stsToken = require('../controllers/oss/stsToken');
 
 module.exports = (socket)=> {
-    console.log('开始')
+
 
     //获得oss读权限的sts临时token
     socket.on('getReadStsToken', stsToken.getReadStsToken());
 
-    console.log('开始')
+
 
     // //获得oss写权限的sts临时token
-    // socket.on('getWriteStsToken', stsToken.getWriteStsToken());
+    socket.on('getWriteStsToken', stsToken.getWriteStsToken());
     
 }
