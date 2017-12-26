dd16d4c3e24e135a0f358b779615a6a1496049e5
panshendeyouhuo <243080462@qq.com>
2017-12-26 16:30:02 +0800

    1

--- app/controllers/user/reg.js
+++ app/controllers/user/reg.js
@@ -12,7 +12,7 @@ Reg.prototype.bossWechatReg = ()=> {
 
         let socket = sio.to(ctx.query.state);
 
-        console.log(socket);
+        console.log(socket.Namespace);
         //关闭微信登入网页
         let html = `
             <script type="text/javascript">
