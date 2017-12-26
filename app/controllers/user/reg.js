618c39b8d6135d92bcd16e3bc9ffdbbc1aed3a8b
panshendeyouhuo <243080462@qq.com>
2017-12-26 17:03:49 +0800

    1

--- app/controllers/user/reg.js
+++ app/controllers/user/reg.js
@@ -12,7 +12,7 @@ Reg.prototype.bossWechatReg = ()=> {
 
         let socket = sio.to(ctx.query.state);
 
-        console.log(socket.sockets.Socket);
+        console.log(socket.connected);
         //关闭微信登入网页
         let html = `
             <script type="text/javascript">
@@ -124,3 +124,26 @@ Reg.prototype.employeeWechatReg = ()=> {
 }
 
 module.exports = new Reg();
+
+
+// { KVNwrrpE0FEt51WIAAAA: 
+//     Socket { 
+//         nsp: Namespace {name: '/',server: [Object],sockets: [Object],connected: [Object],fns: [],ids: 0,rooms: [Array],flags: {},adapter: [Object],_events: [Object],_eventsCount: 1 },
+//         server: Server {nsps: [Object],_path: '/socket.io',_serveClient: true,parser: [Object],encoder: Encoder {}, _adapter: [Function: Adapter],_origins: '*:*',sockets: [Object],eio: [Object],httpServer: [Object],engine: [Object] },
+//         adapter: Adapter {nsp: [Object],rooms: [Object],sids: [Object],encoder: Encoder {} },
+//         id: 'KVNwrrpE0FEt51WIAAAA',
+//         client: Client {server: [Object],conn: [Object],encoder: Encoder {},decoder: [Object],id: 'KVNwrrpE0FEt51WIAAAA',request: [Object],onclose: [Function: bound ],ondata: [Function: bound ],onerror: [Function: bound ],ondecoded: [Function: bound ],sockets: [Object],nsps: [Object],connectBuffer: [] },
+//         conn: Socket {id: 'KVNwrrpE0FEt51WIAAAA',server: [Object],upgrading: false,upgraded: false,readyState: 'open',writeBuffer: [],packetsFn: [],sentCallbackFn: [],cleanupFn: [Array],request: [Object],remoteAddress: '::ffff:118.250.126.238',checkIntervalTimer: null,upgradeTimeoutTimer: null,pingTimeoutTimer: [Object],transport: [Object],_events: [Object],_eventsCount: 3 },
+//         rooms: { KVNwrrpE0FEt51WIAAAA: 'KVNwrrpE0FEt51WIAAAA' },
+//         acks: {},
+//         connected: true,
+//         disconnected: false,
+//         handshake: { headers: [Object],time: 'Tue Dec 26 2017 08:33:27 GMT+0000 (UTC)',address: '::ffff:118.250.126.238',xdomain: true,secure: false,issued: 1514277207424,url: '/socket.io/?EIO=3&transport=websocket',query: [Object] },
+//         fns: [],
+//         flags: {},
+//         _rooms: [],
+//         _events: { disconnect: [Function],authentication: [AsyncFunction],postBossAccount: [AsyncFunction],putBossAccount: [AsyncFunction],deleteBossAccount: [AsyncFunction],tokenLogin: [Function],postProject: [AsyncFunction],getProject: [AsyncFunction],getProjects: [AsyncFunction],putProject: [AsyncFunction],getReadStsToken: [AsyncFunction],getWriteStsToken: [AsyncFunction] },
+//         _eventsCount: 12,
+//         account: { _id: 5a3c987c8b10af001b53511b,nickname: 'pan',sex: 1,province: 'Zhejiang',city: 'Taizhou',country: 'CN',headimgurl: 'http://wx.qlogo.cn/mmopen/vi_32/L8mzhKNSy7Yv3zdicIWYWFiayQXX26owVXbsfVKu7K27jWyPSZMia6IngNIW67pgUxwul3Gzwn9H0sE3xazPfyzVg/0',accessToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjbG91ZC50dWppbmd3YW5nLmNvbSIsImV4cCI6MTUxMzkyMzQxNTU2NywiX2lkIjoiNWEzYzk4N2M4YjEwYWYwMDFiNTM1MTFiIiwiYXV0aG9yaXR5IjpbImFkbWluIl19.gkA9A0Cny8PoMQlPCDPXKGbZ8sBcCBGbjjzLfDHy1cQ=',authority: [Array] } 
+//     } 
+// }
