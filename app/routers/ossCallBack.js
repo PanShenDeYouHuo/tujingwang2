2af130a2e77add88e529d31b411f6b23254ec3e3
panshendeyouhuo <243080462@qq.com>
2018-01-09 20:30:32 +0800

    1

--- app/routers/ossCallBack.js
+++ app/routers/ossCallBack.js
@@ -3,6 +3,7 @@ const router = require('koa-router')();
 const routers = router
 	.get('/', async(ctx, next)=>{
         console.log(ctx);
+        ctx.response.status = 200;
 		ctx.body = 'osscallback';
 	})
 
