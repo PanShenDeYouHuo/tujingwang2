d6b86faf41b9cfa0b62a69e8ed5ad435756aa307
panshendeyouhuo <243080462@qq.com>
2018-01-13 20:17:51 +0800

    1

--- app/controllers/oss/stsToken.js
+++ app/controllers/oss/stsToken.js
@@ -93,48 +93,6 @@ StsToken.prototype.getWriteStsToken = function() {
      
  }
 
-/**
- * 获得oss account写权限的sts临时token
- * 
- * @param {string} uid 账号id
- * @returns {object} stsToken
- */
-StsToken.prototype.getWriteAccountStsToken = function() {
-    return async (uid, fu)=> {
-        try{
-            let policy = {
-                "Statement": [
-                    {
-                        "Action": [
-                            "oss:DeleteObject",
-                            "oss:ListParts",
-                            "oss:AbortMultipartUpload",
-                            "oss:PutObject"
-                        ],
-                        "Effect": "Allow",
-                        "Resource": [
-                            "acs:oss:*:*:tujingcloud/account",
-                            "acs:oss:*:*:tujingcloud/account/*"
-                        ]
-                    }
-                ],
-                "Version": "1"
-            };
-        
-            let arn = 'acs:ram::1647720766129117:role/tujingcloud-write';
-            let sessionName = uid;
-
-            //获取token
-            let token = await this.sts.assumeRole( arn, policy, 60 * 60, sessionName);
-
-            fu(token);
-        } catch (err) {
-            console.log(err);
-            fu({err: true, message: '发生错误'});
-        }
-    }
-     
-}
 
 /**
  * 获得oss account写权限的sts临时token
@@ -156,8 +114,8 @@ StsToken.prototype.getWriteAccountStsToken = function() {
                         ],
                         "Effect": "Allow",
                         "Resource": [
-                            `acs:oss:*:*:tujingcloud/temporaryFile/${uid}`,
-                            `acs:oss:*:*:tujingcloud/temporaryFile/${uid}/*`
+                            `acs:oss:*:*:tujingcloud/temporaryFile/account/${uid}`,
+                            `acs:oss:*:*:tujingcloud/temporaryFile/account/${uid}/*`
                         ]
                     }
                 ],
