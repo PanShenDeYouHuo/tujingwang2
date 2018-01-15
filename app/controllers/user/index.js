42479e952616537dd412366e1b3fb0fc6e447c97
panshendeyouhuo <243080462@qq.com>
2018-01-15 14:26:56 +0800

    1

--- app/controllers/user/index.js
+++ app/controllers/user/index.js
@@ -91,7 +91,6 @@ User.prototype.putRealInformation = function(account) {
             console.log(`temporaryFile/account/${account._id}/`);
             let list = await this.client.list({
                 prefix: 'temporaryFile/',
-                delimiter: '/'
             });
             console.log(list);
             console.log(list.prefixes);
