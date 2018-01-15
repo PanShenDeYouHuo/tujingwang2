5e78420c52a2934881f13b551325d1614c160575
panshendeyouhuo <243080462@qq.com>
2018-01-15 12:01:42 +0800

    1

--- app/controllers/user/index.js
+++ app/controllers/user/index.js
@@ -90,7 +90,7 @@ User.prototype.putRealInformation = function(account) {
         try{
             console.log(`temporaryFile/account/${account._id}/`);
             let list = await this.client.list({
-                marker: `temporaryFile/account/${account._id}/`,
+                marker: 'temporaryFile/account/5a3c987c8b10af001b53511b/',
             });
             console.log(list.prefixes);
             console.log(list.objects);
