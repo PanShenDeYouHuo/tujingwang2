689a268f60fcd3421bf8823a4a2e77ebacee0fe7
panshendeyouhuo <243080462@qq.com>
2018-01-18 15:59:55 +0800

    1

--- app/controllers/user/index.js
+++ app/controllers/user/index.js
@@ -1,5 +1,8 @@
 const user_db = require('../../service/mongodb/m_uesr');
 const OSS = require('ali-oss').Wrapper;                 //Promise函数
+const myio = require('../../sio');
+
+console.log(myio);
 
 function User() {
     this.name = 'user';
