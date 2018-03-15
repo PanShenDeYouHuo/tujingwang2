b0eb05eb7704ca2753f8fb56e22cf31e60f35aac
panshendeyouhuo <243080462@qq.com>
2018-03-15 19:10:36 +0800

    1

--- app/socketRouters/role/service.js
+++ app/socketRouters/role/service.js
@@ -4,5 +4,14 @@ module.exports = (socket)=> {
     
     //创建项目
     socket.on('postProject', project.postProject());
+
+    //根据pid获取项目
+    socket.on('getProject', project.getProject());
+    //根据uid获取项目列表
+    socket.on('getProjects', project.getProjects());
+    
+
+    //保存项目
+    socket.on('putProject', project.putProject());
     
 }
