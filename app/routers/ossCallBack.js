7569408f0e1c5f1646abed99398f11a732bdc138
panshendeyouhuo <243080462@qq.com>
2018-03-26 19:18:05 +0800

    1

--- app/routers/ossCallBack.js
+++ app/routers/ossCallBack.js
@@ -1,35 +1,5 @@
 const router = require('koa-router')();
 
-function parsePostData( ctx ) {
-	return new Promise((resolve, reject) => {
-	  try {
-		let postdata = "";
-		ctx.req.addListener('data', (data) => {
-		  postdata += data
-		})
-		ctx.req.addListener("end",function(){
-			console.log(postdata);
-		  let parseData = parseQueryStr( postdata )
-		  resolve( parseData )
-		})
-	  } catch ( err ) {
-		reject(err)
-	  }
-	})
-  }
-
-  // 将POST请求参数字符串解析成JSON
-function parseQueryStr( queryStr ) {
-	let queryData = {}
-	let queryStrList = queryStr.split('&')
-	console.log( queryStrList )
-	for (  let [ index, queryStr ] of queryStrList.entries()  ) {
-	  let itemList = queryStr.split('=')
-	  queryData[ itemList[0] ] = decodeURIComponent(itemList[1])
-	}
-	return queryData
-  }
-
 const routers = router
 	.get('/', async(ctx, next)=>{
 		ctx.body = 'osscallback';
@@ -41,14 +11,51 @@ const routers = router
 	})
 	.post('/refFileUpload', async(ctx, next)=> {
 		let postData = await parsePostData(ctx);
-		// console.log(ctx.query);
-		// console.log(ctx.request.body);
-		// console.log('post');
+		//拷贝文件
+		console.log(postData.bucket);
+		console.log(postData.object);
+		console.log(postData.size);
+		//保存到数据库
+		//成功返回
 		ctx.body = { postData};
 	})
 	.post('/proFileUpload', async(ctx, next)=> {
 		console.log('post');
+		//拷贝文件
+		//保存到数据库
+		//成功返回
 		ctx.body = { text: 'osscallback'};
 	})
 
 module.exports = routers;
+
+
+// 解析上下文里node原生请求的POST参数
+function parsePostData( ctx ) {
+	return new Promise((resolve, reject) => {
+	  try {
+		let postdata = "";
+		ctx.req.addListener('data', (data) => {
+		  postdata += data
+		})
+		ctx.req.addListener("end",function(){
+			let parseData = parseQueryStr( postdata )
+			resolve( parseData )
+		})
+		} catch ( err ) {
+			reject(err)
+		}
+	})
+}
+
+// 将POST请求参数字符串解析成JSON
+function parseQueryStr( queryStr ) {
+	let queryData = {}
+	let queryStrList = queryStr.split('&')
+	console.log( queryStrList )
+	for (  let [ index, queryStr ] of queryStrList.entries()  ) {
+		let itemList = queryStr.split('=')
+		queryData[ itemList[0] ] = decodeURIComponent(itemList[1])
+	}
+	return queryData
+}
