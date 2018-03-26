b068f616e0849fced2604be441a9dab91b0cd702
panshendeyouhuo <243080462@qq.com>
2018-03-26 14:36:32 +0800

    1

--- app/routers/ossCallBack.js
+++ app/routers/ossCallBack.js
@@ -1,5 +1,22 @@
 const router = require('koa-router')();
 
+function parsePostData( ctx ) {
+	return new Promise((resolve, reject) => {
+	  try {
+		let postdata = "";
+		ctx.req.addListener('data', (data) => {
+		  postdata += data
+		})
+		ctx.req.addListener("end",function(){
+		  let parseData = parseQueryStr( postdata )
+		  resolve( parseData )
+		})
+	  } catch ( err ) {
+		reject(err)
+	  }
+	})
+  }
+
 const routers = router
 	.get('/', async(ctx, next)=>{
 		ctx.body = 'osscallback';
@@ -10,10 +27,11 @@ const routers = router
 		ctx.body = {text:'osscallback'};
 	})
 	.post('/refFileUpload', async(ctx, next)=> {
+		let postData = await parsePostData(ctx);
 		// console.log(ctx.query);
 		// console.log(ctx.request.body);
 		// console.log('post');
-		ctx.body = { text:JSON.stringify(ctx.res)};
+		ctx.body = { postData};
 	})
 	.post('/proFileUpload', async(ctx, next)=> {
 		console.log('post');
