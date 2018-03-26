e1b0dc1ae36bd63c4b13598d9efa950f083ec36a
panshendeyouhuo <243080462@qq.com>
2018-03-26 14:38:29 +0800

    1

--- app/routers/ossCallBack.js
+++ app/routers/ossCallBack.js
@@ -8,6 +8,7 @@ function parsePostData( ctx ) {
 		  postdata += data
 		})
 		ctx.req.addListener("end",function(){
+			console.log(postdata);
 		  let parseData = parseQueryStr( postdata )
 		  resolve( parseData )
 		})
@@ -15,6 +16,18 @@ function parsePostData( ctx ) {
 		reject(err)
 	  }
 	})
+  }
+
+  // 将POST请求参数字符串解析成JSON
+function parseQueryStr( queryStr ) {
+	let queryData = {}
+	let queryStrList = queryStr.split('&')
+	console.log( queryStrList )
+	for (  let [ index, queryStr ] of queryStrList.entries()  ) {
+	  let itemList = queryStr.split('=')
+	  queryData[ itemList[0] ] = decodeURIComponent(itemList[1])
+	}
+	return queryData
   }
 
 const routers = router
