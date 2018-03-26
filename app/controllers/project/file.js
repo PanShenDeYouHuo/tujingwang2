c273366cf63bed445fac9f616ec953bc78d7656a
panshendeyouhuo <243080462@qq.com>
2018-03-26 21:17:07 +0800

    1

--- app/controllers/project/file.js
+++ app/controllers/project/file.js
@@ -1,8 +1,10 @@
+const config = require('../../config')
 const project_db = require('../../service/mongodb/m_project');
 const parsePost  = require('../../modules/parsePostData');
 
 function ProjectFile() {
     this.name = 'projectFile';
+    this.client = config.oss.client;
 }
 /**
  * 添加参考项目文件
@@ -13,16 +15,24 @@ ProjectFile.prototype.refFileUpload = function(){
     return async(ctx, next)=> {
         try {
             let postData = await parsePost(ctx);
-            //拷贝文件
-            console.log(postData.bucket);
-            console.log(postData.object);
-            console.log(postData.size);
+            postData.name = postData.object.substr(postData.object.lastIndexOf('/') + 1);
+            postData.newObject = postData.object.substr(14);
+            //拷贝文件,删除原来的临时文件
+            await this.client.copy(postData.newObject, postData.object);
+            await this.client.delete(postData.object);
             //保存到数据库
+            let referenceFile = {
+                name: postData.name,
+                object: postData.newObject,
+                size: postData.size,
+                bucket: postData.bucket
+            }
+            let res = await project_db.findByIdAndUpdate(postData.pid, {$push: {referenceFile}});
             //成功返回
             ctx.body = {
-                name: postData.object.substr(postData.object.lastIndexOf('/'), postData.object.length), 
+                name: postData.name, 
                 bucket: postData.bucket, 
-                object: postData.object, 
+                object: postData.newObject, 
                 size: postData.size
             };
             
