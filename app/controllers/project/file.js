5bf6ccd5cec1b33f00bd00d1d987c4fbbcbf205d
panshendeyouhuo <243080462@qq.com>
2018-03-26 21:49:27 +0800

    1

--- app/controllers/project/file.js
+++ app/controllers/project/file.js
@@ -15,27 +15,27 @@ ProjectFile.prototype.refFileUpload = function(){
     return async(ctx, next)=> {
         try {
             let postData = await parsePost(ctx);
-            postData.object = postData.object.substring(-1,(postData.object-1));
-            postData.bucket = postData.bucket.substring(-1,(postData.bucket-1));
-            postData.pid = postData.pid.substring(-1,(postData.pid-1));
+            postData.object = postData.object.substr(1,postData.object.length - 1);
+            postData.bucket = postData.bucket.substr(1,(postData.bucket.length-1));
+            postData.pid = postData.pid.substr(1,(postData.pid.length-1));
          
             postData.name = postData.object.substr(postData.object.lastIndexOf('/') + 1);
             postData.newObject = postData.object.substr(14);
             console.log(postData.newObject);
             console.log(postData.object);
             
-            //拷贝文件,删除原来的临时文件
-            await this.client.copy(postData.newObject, postData.object);
-            await this.client.delete(postData.object);
-            //保存到数据库
-            let referenceFile = {
-                name: postData.name,
-                object: postData.newObject,
-                size: postData.size,
-                bucket: postData.bucket
-            }
-            let res = await project_db.findByIdAndUpdate(postData.pid, {$push: {referenceFile}});
-            console.log(res);
+            // //拷贝文件,删除原来的临时文件
+            // await this.client.copy(postData.newObject, postData.object);
+            // await this.client.delete(postData.object);
+            // //保存到数据库
+            // let referenceFile = {
+            //     name: postData.name,
+            //     object: postData.newObject,
+            //     size: postData.size,
+            //     bucket: postData.bucket
+            // }
+            // let res = await project_db.findByIdAndUpdate(postData.pid, {$push: {referenceFile}});
+            // console.log(res);
             //成功返回
             ctx.body = referenceFile;
             
