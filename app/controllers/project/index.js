fd9dc87f8dc687e4d8e79f8dcf027633a4912308
panshendeyouhuo <243080462@qq.com>
2018-04-19 16:17:17 +0800

    1

--- app/controllers/project/index.js
+++ app/controllers/project/index.js
@@ -173,7 +173,7 @@ Project.prototype.pay = (socket)=> {
 
             console.log(project);
             console.log(image);
-            console.log(image.payment + data.money);
+            console.log(image.payment + parseInt(data.money, 10));
             //查看是否结算
             if(image.isSettlement) return fu({err: true, message: '已经结算,无法继续付款'});
 
