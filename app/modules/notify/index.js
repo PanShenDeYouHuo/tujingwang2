3ebd3770491132b917704e860ede013d4a32badc
panshengdeyouhuo <243080462@qq.com>
2018-07-12 17:22:43 +0800

    1

--- app/modules/notify/index.js
+++ app/modules/notify/index.js
@@ -1,15 +1,15 @@
-const sio = require('../../sio');
 const user_db = require('../../service/mongodb/m_uesr');
 
 notify = async(uid, notifyContent)=> {
     console.log('hh')
     console.log(uid)
     console.log(notifyContent);
-    let res = await user_db.findByIdAndUpdate(uid, {$push: {notifyContent}});
+    let res = await user_db.findByIdAndUpdate(uid, {$push: {notify: notifyContent}});
     console.log(res);
     //获取通知用户是否离线
     let user = await user_db.findById(uid, {'_id': 1,'socketId': 1, 'state': 1});
     if (!user.state) return console.log('lixian');
+    const sio = require('../../sio');
     sio.to(user.socketId).emit('notify');
 }
 
