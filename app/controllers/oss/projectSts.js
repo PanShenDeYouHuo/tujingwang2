ecefdf692c0f0c78838f2b3e430569d586e78a57
panshengdeyouhuo <243080462@qq.com>
2018-07-12 20:35:55 +0800

    1

--- app/controllers/oss/projectSts.js
+++ app/controllers/oss/projectSts.js
@@ -139,6 +139,49 @@ ProjectSts.prototype.getWriteAndReadProjectStsToken = function(socket) {
      
 }
 
+/////////////////////////////////////**********全读权限********///////////////////////////////////////////
+ /**
+ * 获得oss account读写权限的sts临时token,没有删除权限
+ * 
+ * @param {string} pid 项目id
+ * @returns {object} stsToken
+ */
+ProjectSts.prototype.getAllWriteAndReadProjectStsToken = function(socket) {
+    return async (data, fu)=> {
+        try{
+            let policy = {
+                "Statement": [
+                    {
+                        "Action": [
+                            "oss:Get*",
+                            "oss:List*"
+                        ],
+                        "Effect": "Allow",
+                        "Resource": [
+                            `acs:oss:*:*:${this.companyName}/project`,
+                            `acs:oss:*:*:${this.companyName}/project/*`,
+                            `acs:oss:*:*:${this.companyName}/temporaryFile/*`
+                        ]
+                    }
+                ],
+                "Version": "1"
+            };
+
+            let arn = 'acs:ram::1647720766129117:role/tujingcloud-readandwrite';
+            let sessionName = socket.account._id.toString();
+            
+            //获取token
+            let token = await this.sts.assumeRole( arn, policy, 60 * 60, sessionName);
+
+            fu(token);
+        } catch (err) {
+            console.log(err);
+            fu({err: true, message: '发生错误'});
+        }
+    }
+     
+}
+
 
 
 module.exports = new ProjectSts();
--- app/socketRouters/public/oss.js
+++ app/socketRouters/public/oss.js
@@ -16,5 +16,6 @@ module.exports = (socket)=> {
     socket.on('getReadProjectStsToken', projectSts.getReadProjectStsToken(socket));
     socket.on('getWriteProjectStsToken', projectSts.getWriteProjectStsToken(socket));
     socket.on('getWriteAndReadProjectStsToken', projectSts.getWriteAndReadProjectStsToken(socket));
+    socket.on('getWriteAndReadProjectStsToken', projectSts.getAllWriteAndReadProjectStsToken(socket));
     
 }
