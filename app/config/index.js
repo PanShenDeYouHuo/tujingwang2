fe71875dea4e40adf12cd045ff0a1c4149a0da28
panshengdeyouhuo <243080462@qq.com>
2018-07-16 21:17:55 +0800

    1

--- app/config/index.js
+++ app/config/index.js
@@ -3,22 +3,41 @@ const STS = OSS.STS;
 
 
 module.exports = {
-    companyName: 'tujingcloud',
+    //测试
+    // companyName: 'tujingcloud',
+    // oss:{
+    //     sts: new STS({
+    //         accessKeyId:'LTAIesg8W64WwrGI',
+    //         accessKeySecret:'3iz2f7iwwGPMoicQE9kQJRPACPOPwK',
+    //     }),
+    //     client: new OSS({
+    //         region:'oss-cn-beijing',
+    //         accessKeyId:'LTAIesg8W64WwrGI',
+    //         accessKeySecret:'3iz2f7iwwGPMoicQE9kQJRPACPOPwK',
+    //         bucket:'tujingcloud',
+    //     }),
+    //     tujingcloudRead: 'acs:ram::1647720766129117:role/tujingcloud-readonly',
+    //     tujingcloudWrite: 'acs:ram::1647720766129117:role/tujingcloud-write',
+    //     tujingcloudReadandWrite: 'acs:ram::1647720766129117:role/tujingcloud-readandwrite',
+    // },
+    //实例
+    companyName: 'tujing',
     oss:{
         sts: new STS({
-            accessKeyId:'LTAIesg8W64WwrGI',
-            accessKeySecret:'3iz2f7iwwGPMoicQE9kQJRPACPOPwK',
+            accessKeyId:'LTAI1xGrNz8uGY3a',
+            accessKeySecret:'0iti686SDosBuoaBYL6h5imevR4IC7',
         }),
         client: new OSS({
             region:'oss-cn-beijing',
-            accessKeyId:'LTAIesg8W64WwrGI',
-            accessKeySecret:'3iz2f7iwwGPMoicQE9kQJRPACPOPwK',
-            bucket:'tujingcloud',
+            accessKeyId:'LTAI1xGrNz8uGY3a',
+            accessKeySecret:'0iti686SDosBuoaBYL6h5imevR4IC7',
+            bucket:'tujing',
         }),
-        tujingcloudRead: 'acs:ram::1647720766129117:role/tujingcloud-readonly',
-        tujingcloudWrite: 'acs:ram::1647720766129117:role/tujingcloud-write',
-        tujingcloudReadandWrite: 'acs:ram::1647720766129117:role/tujingcloud-readandwrite',
+        tujingcloudRead: 'acs:ram::1690867011410501:role/tujing-read',
+        tujingcloudWrite: 'acs:ram::1690867011410501:role/tujing-write',
+        tujingcloudReadandWrite: 'acs:ram::1690867011410501:role/tujing-readandwrite',
     },
+
     wechat:{
         appId:'wx578ee588948c8fcc',
         appSecret:'88666adf25abd7401bcdb5a42f6565e4',
